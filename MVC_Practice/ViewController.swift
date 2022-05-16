a90fb3275aa64f0391bf1d8afbe2e6c1557ace1c
DaunJeong <wjdek88@naver.com>
2022-05-16 18:31:51 +0900

    Initial Commit

---
+++ MVC_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC_Practice
+//
+//  Created by 정다운 on 2022/05/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
